a40201b61d97ddfc2b29a400b54c7c61629757e8
Julie Neury-Ormanni <69806208+Julieneuryormanni@users.noreply.github.com>
2022-02-24 19:25:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \julie.neury.ormanni9@gmail.com\"\n\
-">~/.streamlit/credentials.toml
-
-echo"\
+email = \"julie.neury.ormanni9@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
